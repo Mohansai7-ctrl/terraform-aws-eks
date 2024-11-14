2bc4d4b1c4d709c08e47a7a6a766a0713b851e8f
Mohansai <bandarumohansai@gmail.com>
2024-11-14 22:46:52 +0530

    added https_certificate_arn.value in locals

--- 60-alb/locals.tf
+++ 60-alb/locals.tf
@@ -3,4 +3,5 @@ locals {
     private_subnet_ids = split(",", data.aws_ssm_parameter.private_subnet_ids.value)
     vpc_id = data.aws_ssm_parameter.vpc_id.value
     resource_name = "${var.project_name}-${var.environment}"
+    https_certificate_arn = data.aws_ssm_parameter.https_certificate_arn.value
 }
